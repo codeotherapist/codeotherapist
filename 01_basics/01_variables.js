0344ea6e3b73245da52ff5f0fff06524d0ac5180
codeotherapist <155297371+codeotherapist@users.noreply.github.com>
2023-12-31 11:33:07 +0000

    start done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "codetherapist22@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+//accountId =2
+
+accountEmail ="hc@hc.com"
+accountPassword ="122323"
+accountCity= "Mumbai"
+
+console.log([accountId,accountEmail,accountPassword]);
+console.table([accountEmail,accountPassword,accountCity]);
