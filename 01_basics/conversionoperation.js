0b16230890af40d3b8a193bb412988f8e061bdb4
codeotherapist <155297371+codeotherapist@users.noreply.github.com>
2024-01-01 13:01:20 +0000

    done

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,69 @@
+let score = "codeo"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "codeo"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "codeo"
+let str4 = "therapist"
+let str5 = "friends"
+ str3 = str1 + str2+ str4 + str5
+ //console.log(str3);
+
+ //console.log("1" + 2);
+ //console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+ //console.log( (4+4)*5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3 , num4
+
+num1 = 2
+num2 = 3
+num3 = 5
+num4 = num1+num2+num3
+//console.log(num4)
+let games = 101
+++games;
+//console.log(games);
