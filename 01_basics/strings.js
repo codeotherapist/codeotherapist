b79a7df1565b77eb14418769d2c673f535684cc9
codeotherapist <155297371+codeotherapist@users.noreply.github.com>
2024-01-02 07:17:05 +0000

    math string

---
+++ 01_basics/strings.js
@@ -0,0 +1,32 @@
+const name = "codeo"
+const repoCount = 40
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('codeo-hc-com')
+
+console.log(gameName[0]);
+ //console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0, 3)
+//console.log(newString);
+
+const anotherString = gameName.slice(0, 4)
+console.log(anotherString);
+
+const newStringOne = "  codeo    "
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+//console.log(url.replace('%20', '-'))
+
+//console.log(url.includes('sundar'))
+
+//console.log(gameName.split('-'));
