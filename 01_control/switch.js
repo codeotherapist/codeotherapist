c420d629e2cc70a043809551bee878817301e5be
codeotherapist <155297371+codeotherapist@users.noreply.github.com>
2024-01-04 10:16:30 +0000

    done

---
+++ 01_control/switch.js
@@ -0,0 +1,42 @@
+const month= "jan"
+switch (month){
+    case"jan":
+    console.log("January")
+    break;
+    case"feb":
+    console.log("February")
+    break;
+    case"mar":
+    console.log("March")
+    break;
+    case"apr":
+    console.log("April")
+    break;
+    case"may":
+    console.log("May")
+    break;
+    case"jun":
+    console.log("June")
+    break;
+    case"jul":
+    console.log("July")
+    break;
+    case"aug":
+    console.log("August")
+    break;
+    case"sep":
+    console.log("September")
+    break;
+    case"oct":
+    console.log("October")
+    break;
+    case"nov":
+    console.log("November")
+    break;
+    case"dec":
+    console.log("December")
+    break;
+    default:
+        console.log("default case match");
+        break;
+}
